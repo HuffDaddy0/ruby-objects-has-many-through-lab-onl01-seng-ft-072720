57a22a5da45c08c7ab6fa27e5eb516df264706c7
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 14:41:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,16 +12,16 @@ def self.all
   @@all
 end
 
-def songs
-  Song.all.find_all {|song| song.artist == self}
-end
-
 def new_song(name, genre)
   Song.new(name, self, genre)
 end
 
+def songs
+  Song.all.find_all {|song| song.artist == self}
+end
+
 def genres
-  Genre.artist.find_all {|genre| genre.artist == self}
+  self.songs.map {|song| song.genre}
 end
 
 
