eecd94027e54556baae53a5c59935f17f8adca55
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 15:22:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.find_all {|appointment| @patient == self}
+    Appointment.all.find_all {|appt| appt.patient == self}
   end
 
   def doctors
