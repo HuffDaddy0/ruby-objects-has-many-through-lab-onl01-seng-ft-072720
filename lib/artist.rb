e1a7c515f42aaeee7e6b2bdeb997a597b8db82e7
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 14:20:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+def self.all
+  @@all
+end
+
+def songs
+  Song.all.find {|song| song.artist == self}
+end
+
+def new_song(name, genre)
+  song = Song.new(name, self, genre)
+end
+
+def genres
+  genres = Genre.artist.find_all {|genre| genre.artist == self}
+end
+
+
+
+
+
+
+end
+
+  
