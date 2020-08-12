f0331e909a924af579a02800dea2db769bd7251c
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 14:56:06 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,9 +12,7 @@ def self.all
   @@all
 end
 
-def new_appontment(date, patient)
-  Appointment.new(date, self, patient)
-end
+
 
 def appointments
   Appointment.all.find_all {|song| song.doctor == self}
